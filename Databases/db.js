ae1668ea95048ed3c44d80bc586033f3b28de15a
chhaya bagwan <76725368+chhaya742@users.noreply.github.com>
2022-02-03 11:29:34 +0530

    Update db.js

--- Databases/db.js
+++ Databases/db.js
@@ -2,10 +2,10 @@ require("dotenv")
 const knex = require("knex")({
   client: "mysql",
   connection: {
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASS,
-    database:process.env.DB_NAME
+    host:process.env.MYSQLHOST,
+    user:process.env.MYSQLUSER,
+    password:process.env.MYSQLPASSWORD,
+    database:process.env.MYSQLDATABASE
   },
   useNullAsDefault: true,
 });
